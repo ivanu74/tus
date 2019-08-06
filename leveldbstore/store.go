dcabd54d9a32c8d3bc65f6679ff5d3555b96e105
Ivan Urazov <i.urazov@yadro.com>
2019-08-06 15:11:39 +0300

    Rename import in store.go to this repository

--- leveldbstore/store.go
+++ leveldbstore/store.go
@@ -1,7 +1,7 @@
 package leveldbstore
 
 import (
-	"github.com/eventials/go-tus"
+	"github.com/ivanu74/tus"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
--- memorystore/store.go
+++ memorystore/store.go
@@ -1,7 +1,7 @@
 package memorystore
 
 import (
-	"github.com/eventials/go-tus"
+	"github.com/ivanu74/tus"
 )
 
 // MemoryStore implements an in-memory Store.
